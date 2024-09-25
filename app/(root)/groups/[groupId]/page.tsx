cb6e346e8aedded49c626ba36f8db836cde7d0f5
adam <adamg42685@gmail.com>
2024-09-25 01:20:57 -0400

    fixes to embed on portfolio

--- app/(root)/groups/[groupId]/page.tsx
+++ app/(root)/groups/[groupId]/page.tsx
@@ -6,7 +6,7 @@ import Error from "@/components/shared/Error";
 
 import ContentNotFound from "@/components/shared/ContentNotFound";
 import GroupOverview from "@/components/groups/GroupOverview";
-
+export const dynamic = "force-dynamic";
 const Group = async ({ params }: { params: { groupId: string } }) => {
   const { group, error } = await getGroupById(params.groupId);
 
